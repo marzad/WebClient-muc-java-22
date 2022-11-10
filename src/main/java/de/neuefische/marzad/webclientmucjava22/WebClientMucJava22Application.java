403f5ac2ae3075e399732e31a0abd52a1b5517f9
marzad <maschatz@gmx.net>
2022-11-10 10:57:26 +0100

    Initial commit

---
+++ src/main/java/de/neuefische/marzad/webclientmucjava22/WebClientMucJava22Application.java
@@ -0,0 +1,13 @@
+package de.neuefische.marzad.webclientmucjava22;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebClientMucJava22Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebClientMucJava22Application.class, args);
+	}
+
+}
---
+++ src/test/java/de/neuefische/marzad/webclientmucjava22/WebClientMucJava22ApplicationTests.java
@@ -0,0 +1,13 @@
+package de.neuefische.marzad.webclientmucjava22;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebClientMucJava22ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
